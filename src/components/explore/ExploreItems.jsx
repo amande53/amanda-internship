8ab1c705c9041d0a1ba7502d8f94f76f76314d20
Amanda Anderson <amanderson9@yahoo.com>
2026-09-23 19:03:28 -0400

    Remove unused share and Buy Now overlay from NFT cards

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -60,29 +60,14 @@ const ExploreItems = () => {
 
               <div className="nft__item_wrap">
                 <div className="nft__item_extra">
-                  <div className="nft__item_buttons">
-                    <button>Buy Now</button>
-                    <div className="nft__item_share">
-                      <h4>Share</h4>
-                      <a href="" target="_blank" rel="noreferrer">
-                        <i className="fa fa-facebook fa-lg"></i>
-                      </a>
-                      <a href="" target="_blank" rel="noreferrer">
-                        <i className="fa fa-twitter fa-lg"></i>
-                      </a>
-                      <a href="">
-                        <i className="fa fa-envelope fa-lg"></i>
-                      </a>
-                    </div>
-                  </div>
+                  <Link to={`/item-details/${collection.nftId}`}>
+                    <img
+                      src={collection.nftImage}
+                      className="lazy nft__item_preview"
+                      alt={collection.title}
+                    />
+                  </Link>
                 </div>
-                <Link to={`/item-details/${collection.nftId}`}>
-                  <img
-                    src={collection.nftImage}
-                    className="lazy nft__item_preview"
-                    alt={collection.title}
-                  />
-                </Link>
               </div>
               <div className="nft__item_info">
                 <Link to={`/item-details/${collection.nftId}`}>
